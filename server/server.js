6149cfd82ef3f9bb5e6480d7cae891eb3c325285
Stanlee <stanleyike54@gmail.com>
2023-05-08 00:07:02 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,9 @@ import PartRoute from './Routes/PartRoute.js'
 //Middlewares
 const app = express();
 app.use(express.static('public'));
-app.use(cors());
+app.use(cors({
+    origin: 'https://64582be90554c83d9343ea9d--glittery-panda-4766c1.netlify.app' // or whatever the URL of your React app is
+  }));
 app.use(bodyParser.json({limit: '30mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
 app.use(cookieParser())
